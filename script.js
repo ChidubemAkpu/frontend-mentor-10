f41653902b78c30c1eea1d95e5e0cfcd36d21c20
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-09-04 18:07:49 +0100

    Updated Javascript and css codes

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const headerMenu = document.querySelector('header ul');
+const allBodyEl = document.querySelector('body');
+allBodyEl.addEventListener('mousedown', avoidDefault)
+
 const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', toggleMenu);
-function toggleMenu() {
+
+async function toggleMenu() {
             headerMenu.classList.toggle('open-close')
+            
     }
 
-const [allTheItems, allTheNews] = [document.querySelectorAll('.for-each'), document.querySelectorAll('.each-news')];
+function avoidDefault(el){
+    el.preventDefault();  
+}
+
+const [allTheItems, allTheNews, forFooter] = [document.querySelectorAll('.for-each'), document.querySelectorAll('.each-news'), document.querySelector('footer')];
 document.addEventListener('scroll', getThem)
 
 function getThem(){
     const height = window.innerHeight;
-    [allTheItems, allTheNews].forEach(a => Array.from(a).forEach(b => {
-        if(b.getBoundingClientRect().top < height - 60){
+    [allTheItems, allTheNews,[forFooter]].forEach(a => Array.from(a).forEach(b => {
+        if(b.getBoundingClientRect().top < height - 80){
             b.style.opacity = '1';
         } else{
             b.style.opacity = '0';
